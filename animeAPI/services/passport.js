85ab5ba2d996080a237ca5ad479ee31701e884dc
DeciusWoodson-FS <wsdecius@student.fullsail.edu>
2025-12-18 12:53:32 -0500

    completed back end api auth

--- animeAPI/services/passport.js
+++ animeAPI/services/passport.js
@@ -1,9 +1,38 @@
 const passport = require("passport");
 const ExtractJwt = require("passport-jwt").ExtractJwt;
 const JwtStrategy = require("passport-jwt").Strategy;
+const LocalStrategy = require("passport-local");
 
 const User = require("../models/User");
 const config = require("../config");
+
+const localOptions = {
+  usernameField: "email",
+};
+
+const localStrategy = new LocalStrategy(localOptions, function (
+  email,
+  password,
+  done
+) {
+  User.findOne({ email: email }, function (error, user) {
+    if (error) {
+      return done(error);
+    }
+    if (!user) {
+      return done(null, false);
+    }
+    user.comparePassword(password, function (error, isMatch) {
+      if (error) {
+        return done(error);
+      }
+      if (!isMatch) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  });
+});
 
 const jwtOptions = {
   secretOrKey: config.secret,
@@ -23,4 +52,5 @@ const jwtLogin = new JwtStrategy(jwtOptions, function (payload, done) {
   });
 });
 
+passport.use(localStrategy);
 passport.use(jwtLogin);
