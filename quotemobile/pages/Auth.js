508d6520c6b6fc67a42614caaa7e7c0f8d103fa3
DeciusWoodson-FS <wsdecius@student.fullsail.edu>
2025-12-20 18:44:19 -0500

    updated auth and quote

--- quotemobile/pages/Auth.js
+++ quotemobile/pages/Auth.js
@@ -20,7 +20,7 @@ const getApiUrl = () => {
   return "http://:3000"; // <-- Put IP here
 };
 
-const API_BASE = "http://anime-api-tau-five.vercel.app";
+const API_BASE = "https://anime-api-tau-five.vercel.app";
 
 // ---------------------------------------------------------
 
